c9bed3be71fd463a6baaccc0f01c79954260a151
ez <ez@happys.online>
2021-02-08 23:22:09 +0800

    add pixiv api

--- copyp/urls.py
+++ copyp/urls.py
@@ -1,21 +1,16 @@
-"""copyp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
+from django.conf.urls import url
+ 
+from . import views
+ 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url("user_detail", views.user_detail),
+    url("user_illusts", views.user_illusts),
+    url("illust_detail", views.illust_detail),
+    url("illust_related", views.illust_related),
+    url("illust_related_parse_qs", views.illust_related_parse_qs),
+    url("illust_recommended", views.illust_recommended),
+    url("illust_ranking", views.illust_ranking),
+    url("search_illust", views.search_illust),
+    url("ranking_all", views.ranking_all),
+
 ]
