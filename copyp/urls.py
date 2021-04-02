c79e3607121b0fdebee28474a3714ed226da521e
ez <ez@happys.online>
2021-04-02 18:20:44 +0800

    default
    default

--- copyp/urls.py
+++ copyp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url("user_detail", views.user_detail),
     url("user_illusts", views.user_illusts),
+    url("user_illusts_parse_qs", views.user_illusts_parse_qs),
     url("illust_detail", views.illust_detail),
     url("illust_related", views.illust_related),
     url("illust_related_parse_qs", views.illust_related_parse_qs),
@@ -13,4 +14,7 @@ urlpatterns = [
     url("search_illust", views.search_illust),
     url("ranking_all", views.ranking_all),
     url("image_download", views.image_download),
+    url("ugoira_metadata", views.ugoira_metadata),
+    url("trending_tags_illust", views.trending_tags_illust),
+    
 ]
--- copyp/views.py
+++ copyp/views.py
@@ -1,24 +1,38 @@
 from django.http import HttpResponse
 
+import sys
+from datetime import datetime, timedelta
 from pixivpy3 import *
-
 import json
+
+if sys.version_info >= (3, 0):
+    import imp
+    imp.reload(sys)
+else:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+sys.dont_write_bytecode = True
+
+
+_REFRESH_TOKEN = "FZpHHpYzR-agDJF7okOXqzRyStbnscan73oegMM-FO8"
 
 
 def pixivPassSniAuthApi():
     # api = AppPixivAPI()
-
     api = ByPassSniApi()
     api.require_appapi_hosts(hostname="public-api.secure.pixiv.net")
     api.set_accept_language('zh-cn')
-
-    api.login("lishalom@sina.com", "p6569653")
+    api.auth(refresh_token=_REFRESH_TOKEN)
+    # api.login("lishalom@sina.com", "p6569653")
     return api
 
 
 def pixivAuthApi():
     api = AppPixivAPI()
-    api.login("lishalom@sina.com", "p6569653")
+    api.set_accept_language('zh-cn')
+    api.set_additional_headers(headers="Referer:https://app-api.pixiv.net/")
+    api.auth(refresh_token=_REFRESH_TOKEN)
+    # api.login("lishalom@sina.com", "p6569653")
     return api
 
 
@@ -33,35 +47,43 @@ def pixivApi():
 
 # 获取作者详情
 def user_detail(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.user_detail(request.GET.get('user_id'))
     return HttpResponse(json.dumps(json_result))
 
 
 # 用户作品列表 
 def user_illusts(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.user_illusts(request.GET.get('user_id'))
     return HttpResponse(json.dumps(json_result))
+    
+# 用户作品列表-下一页 (.parse_qs(next_url) 用法)
+def user_illusts_parse_qs(request):
+    api = pixivPassSniAuthApi()
+    next_qs = api.parse_qs(request.GET.get('next_url'))
+    json_result = api.user_illusts(**next_qs)
+    return HttpResponse(json.dumps(json_result))
+
 
 
 # 作品详情 (无需登录，同PAPI.works)
 def illust_detail(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.illust_detail(request.GET.get('illust_id'))
     return HttpResponse(json.dumps(json_result))
 
 
 # 相关作品列表
 def illust_related(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.illust_related(request.GET.get('illust_id'))
     return HttpResponse(json.dumps(json_result))
 
 
 # 作品相关推荐-下一页 (.parse_qs(next_url) 用法)
 def illust_related_parse_qs(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     next_qs = api.parse_qs(request.GET.get('next_url'))
     json_result = api.illust_related(**next_qs)
     return HttpResponse(json.dumps(json_result))
@@ -70,21 +92,21 @@ def illust_related_parse_qs(request):
 # 插画推荐 (Home - Main) 
 # content_type: [illust, manga]
 def illust_recommended(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.illust_recommended()
     return HttpResponse(json.dumps(json_result))
 
 
 # xxxx-xx-xx的过去一周排行
 def illust_ranking(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.illust_ranking('week', date=request.GET.get('date'))
     return HttpResponse(json.dumps(json_result))
 
 
 # 按标签搜索作品
 def search_illust(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.search_illust(request.GET.get('keyword'))
     return HttpResponse(json.dumps(json_result))
 
@@ -104,13 +126,27 @@ def search_illust(request):
 #   female_r18
 #   r18g
 def ranking_all(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.ranking_all(request.GET.get('mode'), 1, 50)
     return HttpResponse(json.dumps(json_result))
 
 
 # 下载图片
 def image_download(request):
-    api = pixivAuthApi()
+    api = pixivPassSniAuthApi()
     json_result = api.download(request.GET.get('image_url'), "", request.GET.get('path'))
+    return HttpResponse(json.dumps(json_result))
+
+
+# 获取图片ZIP信息
+def ugoira_metadata(request):
+    api = pixivPassSniAuthApi()
+    json_result = api.ugoira_metadata(request.GET.get('illust_id'))
+    return HttpResponse(json.dumps(json_result))
+
+
+# 趋势标签 (Search - tags) 
+def trending_tags_illust(request):
+    api = pixivPassSniAuthApi()
+    json_result = api.trending_tags_illust()
     return HttpResponse(json.dumps(json_result))
